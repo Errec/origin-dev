6f655aea3ad835a9f29e9872fb2e47ec3893001f
Raniro Coelho <ranirocoelho@gmail.com>
2024-08-03 14:08:28 -0300

    Config css for card image

--- app/components/layout/section/BlogGrid.tsx
+++ app/components/layout/section/BlogGrid.tsx
@@ -9,20 +9,21 @@ export default async function BlogGrid() {
     const blogData: blogCard[] = await getBlogData();
     console.log(blogData);
     return (
-    <section className="grid grid-cols-1 md:grid-cols-2 mt-4">
+    <section className="rounded-t-xl overflow-hidden grid gap-4 grid-cols-1 md:grid-cols-2 mt-4">
         {blogData.map((post, idx) => (
           <Card key={idx}>
-          {post.titleImage ? (
-            <Image
-                src={urlFor(post.titleImage).url()} 
-                alt="image"
-                height={400}
-                width={400}
-            />
-          ) : (
-            <p>No image available</p>
-          )}
-        </Card>
+            {post.titleImage ? (
+              <Image
+                  src={urlFor(post.titleImage).url()} 
+                  alt="image"
+                  height={400}
+                  width={400}
+                  className="rounded-t-xl h-[400px] w-[100%] object-cover"
+              />
+            ) : (
+              <p>No image available</p>
+            )}
+          </Card>
         ))}
     </section>
   );
