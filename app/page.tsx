f8c7af8ff83693753e91f62213e16653d88a5d5c
Raniro Coelho <ranirocoelho@gmail.com>
2024-08-06 08:54:07 -0300

    Set blog page card list as ul elemente

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,6 @@ import Link from 'next/link';
 export default function Main() {
   return (
     <main>
-      <div className="flex items-center justify-center min-h-screen">
-        <Link href={"/blog"}> Blog </Link>
-      </div>
     </main>
   );
 }
