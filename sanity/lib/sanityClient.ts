a38e410ba1d13269b26c540779e6e26c119b0e22
Raniro Coelho <ranirocoelho@gmail.com>
2024-08-07 10:59:55 -0300

    Update netx env import variable

--- sanity/lib/sanityClient.ts
+++ sanity/lib/sanityClient.ts
@@ -3,8 +3,8 @@ import { createClient } from 'next-sanity';
 
 export const sanityClient = createClient({
     apiVersion: '2021-03-25',
-    dataset: process.env.NEXT_PUBLIC_DATASET || 'production',
-    projectId: 'f93vz2nu',
+    dataset: 'production',
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
     useCdn: false,
 });
 
