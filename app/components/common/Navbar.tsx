ad6613aa3472d6df4e0caadabfba10c777b4ccc6
Raniro Coelho <ranirocoelho@gmail.com>
2024-08-20 20:40:09 -0300

    Add aria label and tooltips

--- app/components/common/Navbar.tsx
+++ app/components/common/Navbar.tsx
@@ -9,21 +9,21 @@ import { ModeToggle } from "./ModeToggle";
 
 export default function Navbar() {
   return (
-    <nav className="w-full flex items-center justify-between mx-auto">
-      <Link href="/" className="font-bold text-3xl dark:text-stone-200 text-stone-100">
+    <nav className="w-full flex items-center justify-between mx-auto" aria-label="Main navigation">
+      <Link href="/" className="font-bold text-3xl dark:text-stone-200 text-stone-100" aria-label="Homepage">
         <div className="hidden sm:block">
-          <Image src={LargeLogo} alt="Large Logo" height={32} priority />
+          <Image src={LargeLogo} alt="Origin Dev Large Logo" height={32} priority />
         </div>
         <div className="block sm:hidden">
-          <Image src={SmallLogo} alt="Small Logo" height={48} priority />
+          <Image src={SmallLogo} alt="Origin Dev Small Logo" height={48} priority />
         </div>
       </Link>
       <div className="flex items-center">
-        <ButtonLink className="mr-4" href="/" icon={<HomeIcon />} />
-        <ButtonLink className="mr-4" href="/projects" icon={<CodeIcon />} />
-        <ButtonLink className="mr-4" href="/blog" icon={<ReaderIcon />} />
-        <ButtonLink className="mr-4" href="/about" icon={<Handshake />} />
-        <ModeToggle />
+        <ButtonLink className="mr-4" tooltip="Home" aria-label="Home" href="/" icon={<HomeIcon />} />
+        <ButtonLink className="mr-4" tooltip="Portfolio" aria-label="Portfolio" href="/projects" icon={<CodeIcon />} />
+        <ButtonLink className="mr-4" tooltip="Blog Posts" aria-label="Blog Posts" href="/blog" icon={<ReaderIcon />} />
+        <ButtonLink className="mr-4" tooltip="Contact Us" aria-label="Contact Us" href="/about" icon={<Handshake />} />
+        <ModeToggle aria-label="Toggle Dark Mode" />
       </div>
     </nav>
   );
