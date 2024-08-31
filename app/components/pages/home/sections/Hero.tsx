e6b510bbf6f510170305730f27d5c03c8a176e0b
Raniro Coelho <ranirocoelho@gmail.com>
2024-08-31 10:49:17 -0300

    Change main element on section hero from div to section add autoprefixer to postcss.config.mjs

--- app/components/pages/home/sections/Hero.tsx
+++ app/components/pages/home/sections/Hero.tsx
@@ -7,7 +7,7 @@ type HeroProps = {
 
 export default function Hero({ heroSection }: HeroProps) {
     return (
-        <div className="relative w-full h-screen">
+        <section className="relative w-full h-screen">
             {heroSection?.backgroundVideo && (
                 <video
                     src={heroSection.backgroundVideo.asset.url}
@@ -40,6 +40,6 @@ export default function Hero({ heroSection }: HeroProps) {
             </div>
         </div>
         </div>
-    </div>
+    </section>
     );
 }
