bf00d1c24fcdd90094638c8247c32e2e3197e329
Raniro Coelho <ranirocoelho@gmail.com>
2024-09-02 18:18:19 -0300

    Update MediaCard component to remove unused state and improve video playback

--- app/components/ui/MediaCard.tsx
+++ app/components/ui/MediaCard.tsx
@@ -3,12 +3,30 @@
 import { ProjectsSection } from '@/types';
 import { gsap } from 'gsap';
 import Image from 'next/image';
-import React, { Suspense, useEffect, useRef, useState } from 'react';
+import React, { Suspense, useEffect, useRef } from 'react';
 
 type Project = ProjectsSection['projects'][number];
 
+const VideoComponent: React.FC<{ hoverVideo: string, videoRef: React.RefObject<HTMLVideoElement> }> = ({ hoverVideo, videoRef }) => {
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play().catch(error => console.error('Error playing video:', error));
+    }
+  }, [videoRef]);
+
+  return (
+    <video
+      ref={videoRef}
+      src={hoverVideo}
+      loop
+      muted
+      playsInline
+      className="absolute inset-0 w-full h-full object-cover transition-opacity duration-600 ease-in-out opacity-0"
+    />
+  );
+};
+
 const MediaCardContent: React.FC<Project> = ({ title, subtitle, image, hoverVideo }) => {
-  const [isHovered, setIsHovered] = useState(false);
   const cardRef = useRef<HTMLDivElement>(null);
   const textContainerRef = useRef<HTMLDivElement>(null);
   const titleRef = useRef<HTMLHeadingElement>(null);
@@ -25,8 +43,6 @@ const MediaCardContent: React.FC<Project> = ({ title, subtitle, image, hoverVide
   }, []);
 
   const handleMouseEnter = () => {
-    setIsHovered(true);
-
     if (imageRef.current && titleRef.current && subtitleRef.current && bgRef.current && videoRef.current) {
       gsap.to(imageRef.current, { opacity: 0, duration: 0.6, ease: "power2.inOut" });
       gsap.to(videoRef.current, { opacity: 1, duration: 0.6, ease: "power2.inOut" });
@@ -41,8 +57,6 @@ const MediaCardContent: React.FC<Project> = ({ title, subtitle, image, hoverVide
   };
 
   const handleMouseLeave = () => {
-    setIsHovered(false);
-
     if (titleRef.current && subtitleRef.current && bgRef.current && imageRef.current && videoRef.current) {
       videoRef.current.pause();
       videoRef.current.currentTime = 0;
@@ -55,7 +69,7 @@ const MediaCardContent: React.FC<Project> = ({ title, subtitle, image, hoverVide
   };
 
   return (
-    <div 
+    <div
       ref={cardRef}
       className="relative w-full aspect-[472/511] overflow-hidden cursor-pointer"
       onMouseEnter={handleMouseEnter}
@@ -71,34 +85,31 @@ const MediaCardContent: React.FC<Project> = ({ title, subtitle, image, hoverVide
           className="object-cover transition-opacity duration-600 ease-in-out"
         />
         {hoverVideo && (
-          <video
-            ref={videoRef}
-            src={hoverVideo.asset.url}
-            loop
-            muted
-            playsInline
-            className="absolute inset-0 w-full h-full object-cover transition-opacity duration-600 ease-in-out opacity-0"
-          />
+          <Suspense
+            fallback={<Image src={image.asset.url} alt={title} fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className="object-cover" />}
+          >
+            <VideoComponent hoverVideo={hoverVideo.asset.url} videoRef={videoRef} />
+          </Suspense>
         )}
       </div>
-      <div 
+      <div
         ref={bgRef}
         className="absolute inset-x-0 bottom-0 bg-black bg-opacity-60 transition-opacity duration-400 ease-in-out opacity-0"
         style={{ height: '20%' }}
       ></div>
-      <div 
+      <div
         ref={textContainerRef}
         className="absolute inset-x-0 bottom-0 px-2 sm:px-4 flex flex-col justify-end items-center"
         style={{ height: '20%' }}
       >
-        <h3 
-          ref={titleRef} 
+        <h3
+          ref={titleRef}
           className="text-white bg-black bg-opacity-60 text-lg sm:text-xl md:text-2xl lg:text-3xl font-light text-center leading-tight p-1 sm:p-2 mb-2 sm:mb-3 md:mb-4 lg:mb-5 w-full transition-transform duration-400 ease-out"
         >
           {title}
         </h3>
-        <p 
-          ref={subtitleRef} 
+        <p
+          ref={subtitleRef}
           className="text-white text-sm sm:text-base md:text-lg lg:text-xl text-center w-full absolute left-0 right-0 bottom-1 sm:bottom-2 opacity-0 px-2 sm:px-4 transition-all duration-400 ease-out"
         >
           {subtitle}
@@ -108,12 +119,4 @@ const MediaCardContent: React.FC<Project> = ({ title, subtitle, image, hoverVide
   );
 };
 
-const MediaCard: React.FC<Project> = (props) => {
-  return (
-    <Suspense fallback={<Image src={props.image.asset.url} alt={props.title} fill sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" className="object-cover" />}>
-      <MediaCardContent {...props} />
-    </Suspense>
-  );
-};
-
-export default MediaCard;
+export default MediaCardContent;
