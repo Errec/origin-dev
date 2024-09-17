90377747c4c021e189f2bc90d2b54c1d342ec061
Raniro Coelho <ranirocoelho@gmail.com>
2024-09-17 09:47:16 -0300

    Refactor UI components to use custom hooks for animation and add dashed separator in Benefits section

--- app/(routes)/home/sections/Benefits.tsx
+++ app/(routes)/home/sections/Benefits.tsx
@@ -16,6 +16,17 @@ const Benefits: React.FC<BenefitsProps> = ({ benefitsSection }) => {
     return IconComponent;
   };
 
+  const DashedSeparator = () => (
+    <div className="w-4/5 mx-auto flex justify-between items-center md:hidden mt-8">
+      {[...Array(10)].map((_, index) => (
+        <React.Fragment key={index}>
+          <div className="w-8 h-[2px] bg-white" />
+          {index < 9 && <div className="flex-grow" />}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+
   return (
     <section className="bg-black text-white pt-24">
       <h2 className="text-4xl md:text-5xl lg:text-6xl font-light text-center mb-16">
@@ -32,15 +43,19 @@ const Benefits: React.FC<BenefitsProps> = ({ benefitsSection }) => {
               <Icon className="w-16 h-16 text-amber-400 mb-8" />
               <p className="text-lg text-center font-light mb-8">{text}</p>
               {index % 3 !== 2 && (
-                <div className="absolute top-8 right-0 w-[1px] h-[calc(100%-64px)] bg-white" />
+                <div className="absolute top-8 right-0 w-[1px] h-[calc(100%-64px)] bg-white hidden md:block" />
               )}
               {index < 3 && (
-                <div className="absolute bottom-0 left-12 right-12 h-[1px] bg-white" />
+                <div className="absolute bottom-0 left-12 right-12 h-[1px] bg-white hidden md:block" />
+              )}
+              {index < benefitsSection.benefits.length - 1 && (
+                <div className="absolute bottom-0 left-[10%] right-[10%] h-[1px] bg-white md:hidden" />
               )}
             </div>
           );
         })}
       </div>
+      <DashedSeparator />
     </section>
   );
 };
