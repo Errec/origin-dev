321f9cbd49e626b1d4c878d324ac01d7c5ee51dc
Raniro Coelho <ranirocoelho@gmail.com>
2024-09-19 20:47:15 -0300

    Refactor about page and add sanity api integration

--- app/api/sanity/index.ts
+++ app/api/sanity/index.ts
@@ -1,4 +1,5 @@
 export { urlFor } from '@/lib/sanity-client';
+export { getAboutPageData } from './about/about';
 export { getArticleData } from './article';
 export { getBlogData } from './blog';
 export { getContactPageData } from './contact/contact';
