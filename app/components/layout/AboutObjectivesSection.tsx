814eea8efb5a71f8cab08a3cea97e9b74ae6daba
Raniro Coelho <ranirocoelho@gmail.com>
2024-09-20 22:41:37 -0300

    Refactor About page to add scrollable content and adjust modal height

--- app/components/layout/AboutObjectivesSection.tsx
+++ app/components/layout/AboutObjectivesSection.tsx
@@ -16,8 +16,8 @@ export default function AboutObjectivesSection({
       className="bg-black flex flex-col md:flex-row h-[80vh] relative"
     >
       {/* Left column: List of objective titles */}
-      <div className="md:w-1/4 p-8 relative">
-        <ul className="space-y-6">
+      <div className="md:w-1/4 py-0 px-8 md:py-8 relative">
+        <ul className="space-y-2 md:space-y-6 ">
           {objectives.map((objective: Objective, index: number) => (
             <li
               key={index}
@@ -27,7 +27,7 @@ export default function AboutObjectivesSection({
               <span
                 className={`transition-all duration-500 ease-in-out ${
                   selectedObjective === index
-                    ? 'text-5xl font-semibold text-amber-400 opacity-100 scale-100 origin-left'
+                    ? 'text-4xl font-semibold text-amber-400 opacity-100 scale-100 origin-left'
                     : 'text-2xl text-amber-400 hover:text-amber-300 opacity-70 scale-75 origin-left'
                 }`}
               >
@@ -36,7 +36,7 @@ export default function AboutObjectivesSection({
             </li>
           ))}
         </ul>
-        <div className="absolute right-6 md:left-4 bottom-6">
+        <div className="absolute right-6 hidden md:block md:left-4 bottom-6">
           <Button
             variant="outline"
             size="sm"
@@ -53,7 +53,7 @@ export default function AboutObjectivesSection({
       </div>
 
       {/* Right column: Selected objective content */}
-      <div className="md:w-3/4 p-8 relative">
+      <div className="md:w-3/4 p-8 relative overflow-scroll">
         <h2
           className="text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl 
           font-light leading-tight"
