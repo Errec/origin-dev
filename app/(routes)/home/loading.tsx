caff42f2af2bc0ce5006f8ead3053966b822c8d2
Raniro Coelho <ranirocoelho@gmail.com>
2024-09-25 12:38:08 -0300

    Update home/loading to consume common compoent

--- app/(routes)/home/loading.tsx
+++ app/(routes)/home/loading.tsx
@@ -1,8 +1,5 @@
+import LoadingAnimation from '@/components/ui/LoadingAnimation';
+
 export default function Loading() {
-  return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black">
-      <div className="text-white text-2xl font-bold z-10">Loading...</div>
-      <div className="absolute bottom-0 left-0 w-full h-1 bg-amber-400 animate-progress" />
-    </div>
-  );
+  return <LoadingAnimation />;
 }
